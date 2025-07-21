fd0241a196960a3c5ae6f4e35b2bf10f1328e0da
SHOKERRR777 <kirillkostrukov2020@gmail.com>
2025-07-21 21:20:30 +0300

    Поменял запуск Flask-файла

--- flask_file.py
+++ flask_file.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -64,4 +65,5 @@ def income():
 
 # Запуск нашей программы
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
